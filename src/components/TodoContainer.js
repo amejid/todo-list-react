080ce6cc096c4d502ba8de65d7e50c90fb5343b5
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-08-08 14:38:37 +0300

    Apply basic styling

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,22 +1,25 @@
 import { PureComponent } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import Header from './Header';
+import InputTodo from './InputTodo';
 import TodosList from './TodosList';
 
 class TodoContainer extends PureComponent {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: 'Setup development environment',
         completed: true,
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: 'Develop website and add content',
         completed: false,
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: 'Deploy to live server',
         completed: false,
       },
@@ -39,22 +42,35 @@ class TodoContainer extends PureComponent {
 
   delTodo = (id) => {
     this.setState({
-      todos: [
-        ...this.state.todos.filter((todo) => todo.id !== id),
-      ],
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
+
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+
+    this.setState({
+      todos: [...this.state.todos, newTodo],
     });
   };
 
   render() {
     return (
-      <>
-        <Header />
-        <TodosList
-          todos={this.state.todos}
-          handleChangeProps={this.handleChange}
-          deleteTodoProps={this.delTodo}
-        />
-      </>
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
+            todos={this.state.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+          />
+        </div>
+      </div>
     );
   }
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,18 +1,30 @@
 import { PureComponent } from 'react';
 
+import styles from './TodoItem.module.scss';
+
 class TodoItem extends PureComponent {
   render() {
+    const completedStyle = {
+      fontStyle: 'italic',
+      color: '#595959',
+      opacity: 0.4,
+      textDecoration: 'line-through',
+    };
+
+    const { completed, id, title } = this.props.todo;
+
     return (
-      <li>
+      <li className={styles.item}>
         <input
           type="checkbox"
-          checked={this.props.todo.completed}
-          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => this.props.handleChangeProps(id)}
         />
-        <button type="button" onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+        <button type="button" onClick={() => this.props.deleteTodoProps(id)}>
           Delete
         </button>
-        {this.props.todo.title}
+        <span style={completed ? completedStyle : null}>{title}</span>
       </li>
     );
   }
