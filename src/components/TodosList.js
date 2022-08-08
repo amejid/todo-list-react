0c03d6ea83c73ee69a1cbd1c29bdbd5ced3973fb
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-08-08 16:48:55 +0300

    Refactor with hooks

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,22 +1,17 @@
-import { PureComponent } from 'react';
 import TodoItem from './TodoItem';
 
-class TodosList extends PureComponent {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            handleChangeProps={this.props.handleChangeProps}
-            deleteTodoProps={this.props.deleteTodoProps}
-            setUpdate={this.props.setUpdate}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
+const TodosList = (props) => (
+  <ul>
+    {props.todos.map((todo) => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        handleChangeProps={props.handleChangeProps}
+        deleteTodoProps={props.deleteTodoProps}
+        setUpdate={props.setUpdate}
+      />
+    ))}
+  </ul>
+);
 
 export default TodosList;
