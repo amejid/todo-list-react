e90d0224518f4161f39376e29cd434569da9e08c
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-08-08 11:48:25 +0300

    Render list of todos

---
+++ src/components/TodosList.js
@@ -0,0 +1,21 @@
+import { PureComponent } from 'react';
+import TodoItem from './TodoItem';
+
+class TodosList extends PureComponent {
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={this.props.handleChangeProps}
+            deleteTodoProps={this.props.deleteTodoProps}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TodosList;
